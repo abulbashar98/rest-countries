8aba2e3b5aae05efbb8388f48118801b7f1cb7f0
abulbashar98 <abulbashar98116@gmail.com>
2022-03-14 19:50:50 +0600

    destructuring-props-attribute

--- js/countries.js
+++ js/countries.js
@@ -18,14 +18,26 @@ const displayCountries = countries => {
 
 loadCountries();
 
+// Note: we could have sent only required items or from displayCountries() function and use them via taking parameters in getCountryHtml() function......
+
+// const getCountryHtml = name, country, capital => {
 const getCountryHtml = country => {
 
 
+    const name = country.name.official;
+    const capital = country.capital;
+    const flag = country.flags.png;
+
+
+    /* Note: Or we could have used destructuring like const {name, capital, flags}
+    = country;  if only all these data were in the same layer....
+    */
+
     return `
-             <div class="country">
-             <h2>${country.name.official}</h2>
-             <h3>Capital: ${country.capital}</h3>
-             <img src="${country.flags.png}">
-              </div>
-     `;
+         <div class="country">
+         <h2>${name}</h2>
+         <h3>Capital: ${capital}</h3>
+         <img src="${flag}">
+          </div>
+ `;
 }
